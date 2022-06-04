46071b260fc6e590e15091cdbd0f1ad9d749273b
messaoudhamrioui1 <messaoud.hamrioui@gmail.com>
2022-06-04 11:28:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-luvv1f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-luvv1f)
---
+++ index.js
@@ -0,0 +1,19 @@
+const https = require('https');
+const options = {
+  hostname: 'google.com',
+  method: 'GET',
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.end();
